aed6778638828bb1160bd471738b96eace755e56
davidby8 <dby0806@gmail.com>
2025-06-01 19:25:57 +0200

    registro

---
+++ registre.php
@@ -0,0 +1,15 @@
+<?php include "config.php";
+if ($_POST) {
+  $nom = $_POST['nom'];
+  $email = $_POST['email'];
+  $pass = password_hash($_POST['pass'], PASSWORD_DEFAULT);
+  $conn->query("INSERT INTO usuaris (nom, email, password) VALUES ('$nom','$email','$pass')");
+  header("Location: login.php");
+}
+?>
+<form method="POST">
+  Nom: <input name="nom"><br>
+  Email: <input name="email"><br>
+  Contrasenya: <input type="password" name="pass"><br>
+  <button>Registrar</button>
+</form>
